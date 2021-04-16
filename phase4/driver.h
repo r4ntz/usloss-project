925f3849beaf20f5624d56f5808211c54842e372
r4ntz <rantzm@email.arizona.edu>
2021-04-16 16:07:40 -0700

    name, pid, and status attrs for driver_proc

--- phase4/driver.h
+++ phase4/driver.h
@@ -1,22 +1,27 @@
+#define DEBUG4 1
 
+#define ACTIVE 1
 
 typedef struct driver_proc * driver_proc_ptr;
 
-struct driver_proc {
-   driver_proc_ptr next_ptr;
+struct driver_proc
+{
+        driver_proc_ptr next_ptr;
 
-   int   wake_time;    /* for sleep syscall */
-   int   been_zapped;
+        int wake_time; /* for sleep syscall */
+        int been_zapped;
 
 
-   /* Used for disk requests */
-   int   operation;    /* DISK_READ, DISK_WRITE, DISK_SEEK, DISK_TRACKS */
-   int   track_start;
-   int   sector_start;
-   int   num_sectors;
-   void *disk_buf;
+        /* Used for disk requests */
+        int operation; /* DISK_READ, DISK_WRITE, DISK_SEEK, DISK_TRACKS */
+        int track_start;
+        int sector_start;
+        int num_sectors;
+        void *disk_buf;
 
-   //more fields to add
+        //more fields to add
+        char name[MAXNAME];
+        int pid;
+        int status;
 
 };
-
